8b52d3574e6fcdf72825e4a3afafaef386a43a07
Nayana Davis <nayana487@gmail.com>
2016-07-03 21:26:03 -0400

    added comments

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Bonus points for using JS! Code here looks good!
+// Just a small thought, but since you only have one "menu" item and you're only trying to perform
+// an action on that one element, it's easier to write this as a getElementById.
+
 function dropdown() {
     document.getElementsByClassName("menu")[0].classList.toggle("responsive");
 }
